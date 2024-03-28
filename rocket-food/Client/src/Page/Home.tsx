ef0dfddfbb32cf6ed3d4f5e674cf50beb7c2b217
Rexchris <92377271+Rexchris@users.noreply.github.com>
2024-03-28 12:08:20 +0100

    Début de l'accueil

--- rocket-food/Client/src/Page/Home.tsx
+++ rocket-food/Client/src/Page/Home.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import './Home.css';
+import PizzaMan from '../Image/AstroPizzaRed.png';
+import {Col, Row} from "antd";
 
 function Home() {
     return (
         <div className="Home">
             <header className="Home-header">
-                <Header />
+                <Header/>
             </header>
-
+            <body>
+            <Row wrap={true} align={"middle"} justify={"center"}>
+                <Col pull={5}>
+                    <h1>
+                        Be The Fastest In Delivering Your Food
+                    </h1>
+                </Col>
+                <Col pull={10}>
+                    <img src={PizzaMan} alt="PizzaMan" className="RoundedImage"/>
+                </Col>
+            </Row>
+            </body>
             <footer>
-                <Footer />
+                <Footer/>
             </footer>
         </div>
     );
