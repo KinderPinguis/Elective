fa9b2541fb5cafa56d7c6af62894390239bcaf15
Yoann Miglionico--Jacquot <62853008+KinderPinguis@users.noreply.github.com>
2024-04-04 16:31:57 +0200

    Add CRUD and LogIn User

--- rocket-food/User/Middlewares/AuthMiddleware.js
+++ rocket-food/User/Middlewares/AuthMiddleware.js
@@ -1,12 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+const { Request, Response } = require('express');
+const jwt = require('jsonwebtoken');
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+exports.authMiddleware = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) return res.status(401).json({ message: 'Authorization token is required' });
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || '');
+        const decoded = jwt.verify(token, process.env.ACCESS_JWT_KEY || '');
         req.user = decoded;
         next();
     } catch (error) {
